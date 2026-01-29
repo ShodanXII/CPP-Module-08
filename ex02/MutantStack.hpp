fec5757255e1330b38e70dee3b4792a38fb0a083
ShodanXII <156700077+ShodanXII@users.noreply.github.com>
2026-01-29 19:54:19 +0000

    lol

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -7,17 +7,17 @@ template<typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    MutantStack( void );
-    MutantStack( const MutantStack& copy );
-    MutantStack& operator=( const MutantStack& other );
-    ~MutantStack( void );
-    typedef typename std::stack<T>::container_type::iterator iterator;
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-    
-    iterator begin( void );
-    iterator end( void );
-    const_iterator begin( void ) const;
-    const_iterator end( void ) const;
+	MutantStack( void );
+	MutantStack( const MutantStack& copy );
+	MutantStack& operator=( const MutantStack& other );
+	~MutantStack( void );
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	
+	iterator begin( void );
+	iterator end( void );
+	const_iterator begin( void ) const;
+	const_iterator end( void ) const;
 };
 
 #include "MutantStack.tpp"
